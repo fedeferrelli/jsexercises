20e3f4fab6d6c4e18a8401518d8e7d697639f2bf
Fede <ferrelli.mazza@gmail.com>
2022-09-16 07:54:00 -0300

    Add HashRouter from reacr-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Components/Home";
 import Palindrome from "./Components/palindromo/Palindrome";
