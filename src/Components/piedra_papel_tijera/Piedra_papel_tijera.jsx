6dad0a02e887bb808b061c9d15e325bd78575a72
Fede <ferrelli.mazza@gmail.com>
2022-09-27 22:29:04 -0300

    Add counting games to Piedra, Papel o Tijera

--- src/Components/piedra_papel_tijera/Piedra_papel_tijera.jsx
+++ src/Components/piedra_papel_tijera/Piedra_papel_tijera.jsx
@@ -183,18 +183,18 @@ function Piedra_papel_tijera() {
 
               <div className="text-gray-700 text-xl p-3 flex flex-row gap-2 justify-evenly">
              
-              <div className={machineScore<humanScore ? "w-1/3 rounded-lg p-2 border border-green-600 bg-green-500" : machineScore==humanScore ? "rounded-lg p-2 border border-gray-800 w-1/3 " :"p-2 rounded-lg w-1/3  bg-red-400 border border-red-600"}>
+              <div className={machineScore<humanScore ? "w-1/3 rounded-lg p-2 border border-green-600 bg-green-500" : machineScore==humanScore ? "rounded-lg p-2 border border-gray-800 w-1/3 " :"p-2 rounded-lg w-1/3  bg-red-400 border border-red-500"}>
                 Tú: <span className="block">{humanScore}</span>
               </div> 
              
-           <div className={machineScore>humanScore ? "w-1/3  rounded-lg p-2 border-green-600 border bg-green-500" : machineScore==humanScore ? "rounded-lg p-2 border border-gray-800 w-1/3 " :"w-1/3  p-2 rounded-lg bg-red-400 border border-red-600"}>
+           <div className={machineScore>humanScore ? "w-1/3  rounded-lg p-2 border-green-600 border bg-green-500" : machineScore==humanScore ? "rounded-lg p-2 border border-gray-800 w-1/3 " :"w-1/3  p-2 rounded-lg bg-red-400 border border-red-500"}>
              Machine: <span className="block">{machineScore}</span>
               </div>
               
               
               </div>
 
-              <div className="text-md text-gray-600 py-2 font-thin ">{countGames} {countGames===1 ? 'juego' : 'juegos'} </div>
+              <div className="text-md text-gray-700 py-2 font-thin ">{countGames} {countGames===1 ? 'juego' : 'juegos'} </div>
               
             </div>
 
